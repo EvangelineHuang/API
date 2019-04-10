a722e7485356f0e415811578fc51376f31eac99a
Evangeline <Evangeline@appledeMacBook.local>
2019-04-09 20:35:39 -0400

    extra

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ malesP.then(function(d)
     .attr("href",function(d)
     {return d.url;})
     .text(function(d)
-
     {return d.name});
+
   d3.select('#numMales')
       .append("td")
       .text(numMales);
@@ -40,7 +40,6 @@ speciesP.then(function(d)
 {
   var listOfSpecies = []
   var i = 0
-  console.log(d[i].classification);
   while(i<d.length)
   {
   if (!listOfSpecies.includes(d[i].classification))
@@ -52,9 +51,37 @@ speciesP.then(function(d)
   i++;
   }
   var numSpecies = listOfSpecies.length;
-  console.log(listOfSpecies);
   d3.select('#numSpecies')
         .append("td")
         .text(numSpecies);
 
 })
+//////////////////////////////
+var people=d3.json("https://ghibliapi.herokuapp.com/people")
+people.then(function(data){
+  var p=data.map(function(ele){
+    return[ele.name,ele.gender,ele.age,ele.eye_color,ele.hair_color]
+  })
+  var title=["name","gender","age","eye_color","hair_color"]
+  var table=d3.select("body")
+    .append("div")
+    .append("table")
+    .classed("chart",true)
+  table.append("thead")
+       .append("tr")
+       .selectAll("th")
+       .data(title)
+       .enter()
+       .append("th")
+       .text(function(d){return d})
+  table.append("tbody")
+       .selectAll("t")
+       .data(p)
+       .enter()
+       .append("tr")
+       .selectAll("td")
+       .data(function(d){return d})
+       .enter()
+       .append("td")
+       .text(function(d){return d})
+},function(err){console.log(err)})
